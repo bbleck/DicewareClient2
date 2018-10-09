2b40013fa5fcf258124b9afb0ac7460f1614a2e6
brianb <bbleck@unm.edu>
2018-10-09 14:49:15 -0600

    Added text input fields, guidelines. Hooked them into functionality.

--- app/src/main/java/edu/cnm/deepdive/dicewareclient2/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/dicewareclient2/MainActivity.java
@@ -1,14 +1,19 @@
 package edu.cnm.deepdive.dicewareclient2;
 
 import android.os.AsyncTask;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import java.io.IOException;
@@ -24,6 +29,8 @@ public class MainActivity extends AppCompatActivity {
   private ListView words;
   private Button generate;
   private DicewareService service;
+  private ProgressBar progBar;
+  private TextInputEditText length;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -48,11 +55,36 @@ public class MainActivity extends AppCompatActivity {
   private void initViews() {
     words = findViewById(R.id.lvWords);
     generate = findViewById(R.id.btnGenerate);
+    progBar = findViewById(R.id.pbSpinner);
+    length = findViewById(R.id.etLength);
     generate.setOnClickListener(new OnClickListener() {
       @Override
       public void onClick(View v) {
         new GenerateTask().execute();
         generate.setClickable(false);
+      }
+    });
+    length.addTextChangedListener(new TextWatcher() {
+      CharSequence before;
+      @Override
+      public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        before = s;
+      }
+
+      @Override
+      public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+      }
+
+      @Override
+      public void afterTextChanged(Editable s) {
+        try{
+          Integer.parseInt(s.toString());
+        }catch(NumberFormatException e){
+          length.removeTextChangedListener(this);
+          length.setText(before);
+          length.addTextChangedListener(this);
+        }
       }
     });
   }
@@ -61,10 +93,15 @@ public class MainActivity extends AppCompatActivity {
   private class GenerateTask extends AsyncTask<Void, Void, String[]> {
 
     @Override
+    protected void onPreExecute() {
+      progBar.setVisibility(View.VISIBLE);
+    }
+
+    @Override
     protected String[] doInBackground(Void... voids) {
       String[] passphrase = null;
       try {
-        Call<String[]> call = service.get(6);
+        Call<String[]> call = service.get(Integer.parseInt(length.getText().toString()));
         Response<String[]> response = call.execute();
         if(response.isSuccessful()){//200 range is success, 300 is redirect, 400 is you did something wrong, 500 is server is doing something wrong
           passphrase = response.body();
@@ -84,8 +121,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onPostExecute(String[] strings) {
       ArrayAdapter<String> adapter =
           new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, strings);
+      progBar.setVisibility(View.INVISIBLE);
       words.setAdapter(adapter);
       generate.setClickable(true);
+    }
+
+    @Override
+    protected void onCancelled(String[] strings) {
+      progBar.setVisibility(View.INVISIBLE);
+      generate.setClickable(true);
+      Toast.makeText(MainActivity.this, "Unable to obtain passphrase.", Toast.LENGTH_SHORT).show();
     }
   }
 
